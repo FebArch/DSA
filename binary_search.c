649d367724fcd14137acf82ec32d1c0e9862739c
FebArch <febarchapray@gmail.com>
2026-07-14 18:26:08 +0530

    binary search small fix

--- binary_search.c
+++ binary_search.c
@@ -3,7 +3,7 @@
 int binarySearch(int* ptr,int size, int element);
 
 int main(){
-    int element = 2;
+    int element = 9;
     int arr[10] = {1,2,3,4,5,6,7,8,9};
     int index = binarySearch(arr, 9, element);
     printf("Index of %d is %d\n", element, index);
@@ -13,17 +13,20 @@ int main(){
 
 int binarySearch(int* ptr,int size, int element){
     // 1 2 3 4 5 6 7 8 9;  // 7    
-    int firstElement = 0, lastElement = size-1;
+    int firstIndex = 0, lastIndex = size-1;
 
-    for (int i = 0; i < lastElement; i = (int) (firstElement+lastElement)/2)
+    for (int i = 0; i < lastIndex; i = (int) (firstIndex+lastIndex)/2)
     {
         if (ptr[i] == element)    // 0     4       //  6     
         {
             return i;
-        }else if(ptr[i] < element){
-            firstElement = i; //            0               4                  6
+        }else if(ptr[size-1] == element){
+            return (size-1);
+        }
+        else if(ptr[i] < element){
+            firstIndex = i; //            0               4                  6
         }else if(ptr[i] > element){
-            lastElement = i;
+            lastIndex = i;
         }        
     }
     
