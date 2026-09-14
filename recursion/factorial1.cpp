347c3ba2eb84aba8f7523e3f5669c900856dc61e
FebArch <febarchapray@gmail.com>
2026-09-14 18:13:59 +0530

    factorial implemented again

---
+++ recursion/factorial1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    if (n==1)
+    {
+        return 1;
+    }
+    return n * factorial(n-1);
+};
+
+int main(){
+    int number;
+    cout << "Enter Your NUmber: ";
+    cin >> number;
+
+    int result = factorial(number);
+    cout << "Factorial of " << number << " is " << result << endl;
+}
