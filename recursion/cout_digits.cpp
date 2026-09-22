608890a85c5cd69c819b27acc033f97517f7f1f1
FebArch <febarchapray@gmail.com>
2026-09-22 10:13:29 +0530

    counting digits using recursion

---
+++ recursion/cout_digits.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int count_digits(int n);
+
+int main() {
+    int num = -1987;
+    cout << "Number of digits in " << num << " is " << count_digits(num) << endl;
+    return 0;
+}
+
+int count_digits(int n){
+    if ((n/10) == 0)
+    {
+        return 1;
+    }
+    
+    int count = 1;
+    return count + count_digits(n/10);
+}
